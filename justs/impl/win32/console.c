16cf610bd046ca0c8fb31c4b4767f19643f20ba5
Fillirini <fillirini71@mail.ru>
2024-12-16 17:16:52 +0300

    Update ConsoleAPI
    adding the ability to output UTF8 strings.
    Now you can output UTF8 and UTF16_LE encoded strings in C-Style or standard strings/slices that store their length.

--- justs/impl/win32/console.c
+++ justs/impl/win32/console.c
@@ -2,10 +2,49 @@
 
 #include <windows.h>
 
-void print(const wchar_t* cstring) {
-    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), cstring, wcslen(cstring), 0, 0);
+
+void initConsole() {
+    SetConsoleOutputCP(CP_UTF8);
 }
-void println(const wchar_t* cstring) {
-    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), cstring, wcslen(cstring), 0, 0);
-    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), "\n", 1, 0, 0);
+
+
+
+void print16(const wchar_t* cstring) {
+    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), cstring, wcslen(cstring), NULL, NULL);
+}
+
+void println16(const wchar_t* cstring) {
+    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), cstring, wcslen(cstring), NULL, NULL);
+    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), L"\n", 1, NULL, NULL);
+}
+
+
+void sprint16(const unsigned short* string, size_t length) {
+    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), string, length, NULL, NULL);
+}
+
+void sprintln16(const unsigned short* string, size_t length) {
+    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), string, length, NULL, NULL);
+    WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), L"\n", 1, NULL, NULL);
+}
+
+
+
+void print8(const unsigned char* cstring) {
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), cstring, strlen(cstring), NULL, NULL);
+}
+
+void println8(const unsigned char* cstring) {
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), cstring, strlen(cstring), NULL, NULL);
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), u"\n", 1, NULL, NULL);
+}
+
+
+void sprint8(const unsigned char* string, size_t length) {
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), string, length, NULL, NULL);
+}
+
+void sprintln8(const unsigned char* string, size_t length) {
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), string, length, NULL, NULL);
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), u"\n", 1, NULL, NULL);
 }
--- justs/src/console.h
+++ justs/src/console.h
@@ -1,7 +1,22 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
-void print(const unsigned short* cstring);
-void println(const unsigned short* cstring);
+// output utf16 string
+
+void initConsole();
+
+void print16(const unsigned short* cstring);
+void println16(const unsigned short* cstring);
+
+void sprint16(const unsigned short* string, size_t length);
+void sprintln16(const unsigned short* string, size_t length);
+
+// output utf8 string
+
+void print8(const unsigned char* cstring);
+void println8(const unsigned char* cstring);
+
+void sprint8(const unsigned char* string, size_t length);
+void sprintln8(const unsigned char* string, size_t length);
 
 #endif // CONSOLE_H
